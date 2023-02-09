b7076e7ceef087fc01cc6ffc9f2497111f869805
ranaintizar <intizaralirana2@gmail.com>
2023-02-10 00:51:57 +0500

    Minor Fix

--- src/components/careers-page/CareersPage.js
+++ src/components/careers-page/CareersPage.js
@@ -5,21 +5,12 @@ import JobSection from "@components/careers-sections/job-section";
 import BenefitSection from "@components/careers-sections/benefit-section";
 import ValuesSection from "@components/careers-sections/our-values-section";
 
-import BGImg from "@assets/careers-2.jpg";
-
 import stl from "./CareersPage.module.scss";
 
 const CareersPage = () => {
   return (
     <div className={stl.careersPage}>
-      <Intro
-        imgSrc={`${BGImg.src}`}
-        alt="backgroun-image-careers"
-        heading="World class design, agile delivery and product development"
-        desc="200 experts who have served more than 500 international clients. Let's
-        bring your idea to life!"
-        icon={<ArrowNext />}
-      />
+      <Intro />
       <UpperSection />
       <WeAreHiring />
       <JobSection />
