78225d9595af34a81b711585404b3984353078d0
Rana Intizar <110844434+ranaintizar@users.noreply.github.com>
2023-02-17 22:12:29 +0500

    Add How-We-Work Page (#16)
    Add How We Work Page

--- src/components/upper-section-container/UpperSecCont.js
+++ src/components/upper-section-container/UpperSecCont.js
@@ -1,11 +1,5 @@
 import { upperSection } from "@components/data/careers-data";
 
-import stl from "./UpperSecCont.module.scss";
-
-const UpperSecCont = () => (
-  <div className={stl.upperSecCont}>
-    {upperSection.map((section) => section)}
-  </div>
-);
+const UpperSecCont = () => <div>{upperSection.map((section) => section)}</div>;
 
 export default UpperSecCont;
