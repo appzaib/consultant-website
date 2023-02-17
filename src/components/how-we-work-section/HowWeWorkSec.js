78d85e8566c00a85f536f26a819dab19df1e015d
ranaintizar <intizaralirana2@gmail.com>
2023-02-17 22:37:04 +0500

    Minor Fix

---
+++ src/components/how-we-work-section/HowWeWorkSec.js
@@ -0,0 +1,11 @@
+import stl from "./HowWeWorkSec.module.scss";
+
+const HowWeWorkSec = () => {
+  return (
+    <div className={stl.howWeWorkSec}>
+      <h1>How We Work Section</h1>
+    </div>
+  );
+};
+
+export default HowWeWorkSec;
